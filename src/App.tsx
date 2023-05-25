1affd6a9b5c3ced7e8f4e22199719df015c5e1ef
Victoria <124435946+askthedust1@users.noreply.github.com>
2023-05-26 00:15:41 +0600

    создала функцию createItems

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const createItems = () => {
+    const arr = [];
+
+    for (let i = 0; i < 36; i++) {
+      const info = {hasItem: false, clicked: false, id: i};
+      arr.push(info);
+    }
+
+    let random = Math.floor(Math.random() * 36);
+    arr[random] = {hasItem: true, clicked: false, id: random}
+
+    console.log(arr);
+  };
+
+  const [items, setItems] = useState(createItems());
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
